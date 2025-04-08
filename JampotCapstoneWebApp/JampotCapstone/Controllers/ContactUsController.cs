4a0354207f03d2a2c263a552f280b1798af38057
Gabe Gucker <guckerg.my.lanecc.edu>
2025-04-08 15:36:28 -0700

    Added ContactUsController, View, Messages Model, and navigation. Began Form for contact submission.

---
+++ JampotCapstoneWebApp/JampotCapstone/Controllers/ContactUsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JampotCapstone.Models;
+
+namespace JampotCapstone.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void SendMessage()
+        {
+            //CRUD for contact message
+        }
+    }
+}
---
+++ JampotCapstoneWebApp/JampotCapstone/Models/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JampotCapstone.Models
+{
+    public class Messages
+    {
+        public int MessageId { get; set; }
+        [StringLength(50), Required]
+        public string Sender { get; set; } = "";
+        public int PhoneNumber { get; set; }
+
+        [RegularExpression(@"^[A-Za-z0-9\.]+@[A-Za-z0-9\.]+$")]
+        [StringLength(50)]
+        public string Email { get; set; } = "";
+
+        [StringLength(50)] public string Subject { get; set; } = "";
+        public string MessageText { get; set; } = "";
+
+        //uncomment when AppUser is added
+        //public AppUser? Customer { get; set; }
+    }
+}
