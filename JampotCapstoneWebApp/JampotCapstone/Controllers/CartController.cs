20e166feae40f5de29c45f012a42800b606c0835
DershamE <eiladersham@gmail.com>
2025-04-14 13:22:23 -0700

    Basic styling added.

--- JampotCapstoneWebApp/JampotCapstone/Controllers/CartController.cs
+++ JampotCapstoneWebApp/JampotCapstone/Controllers/CartController.cs
@@ -9,18 +9,12 @@ namespace JampotCapstone.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public CartController(ApplicationDbContext context) 
-        {
+        public CartController(ApplicationDbContext context) {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            List<ProductType> model = await _context.ProductTypes
-                .Include(c => c.Products)
-                    .ThenInclude(p => p.Tags)
-                .Include(c => c.Products)
-                    .ThenInclude(p => p.ProductPhoto).ToListAsync();
-            return View(model);
+            return View();
         }
     }
 }
