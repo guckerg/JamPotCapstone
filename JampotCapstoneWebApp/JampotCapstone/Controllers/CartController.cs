759788e4e5520ec1b5a549e021d11decd7ff9a2f
DershamE <eiladersham@gmail.com>
2025-04-13 19:09:17 -0700

    Added view and controller for cart. Minimal styling added

---
+++ JampotCapstoneWebApp/JampotCapstone/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using JampotCapstone.Data;
+using JampotCapstone.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JampotCapstone.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<ProductType> model = await _context.ProductTypes
+                .Include(c => c.Products)
+                    .ThenInclude(p => p.Tags)
+                .Include(c => c.Products)
+                    .ThenInclude(p => p.ProductPhoto).ToListAsync();
+            return View(model);
+        }
+    }
+}
