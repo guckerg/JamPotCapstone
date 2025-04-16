6c4f6485e6c7acf71a4ecd7c49057d746f8cdb4d
DershamE <eiladersham@gmail.com>
2025-04-16 14:46:45 -0700

    Added Order and OrderItem classes

---
+++ JampotCapstoneWebApp/JampotCapstone/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JampotCapstone.Models
+{
+    public class Order
+    {
+        public int OrderId {  get; set; }  
+        public int SubTotal { get; set; }
+
+        [StringLength(50), Required]
+        public string CustomerName { get; set; } = "";
+        public ICollection<OrderItem> OrderItems { get; set; }
+
+    }
+}
---
+++ JampotCapstoneWebApp/JampotCapstone/Models/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace JampotCapstone.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
