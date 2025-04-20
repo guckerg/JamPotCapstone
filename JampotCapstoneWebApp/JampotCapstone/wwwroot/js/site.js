59c0f5c7ab0858903ca10c11bcc910fa12abfa9a
Rosa Myers <larosaardente@gmail.com>
2025-04-20 13:57:00 -0700

    Took a run at the problem of hiding the careers link with javascript.

--- JampotCapstoneWebApp/JampotCapstone/wwwroot/js/site.js
+++ JampotCapstoneWebApp/JampotCapstone/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById("toggle").onclick = () => {
+    const careers = document.getElementById("careers");
+    const toggle = document.getElementById("toggle");
+    let status = careers.style.display === "none" ? "show" : "hide";
+    if (status === "hide") {
+        careers.style.display = "none";
+        toggle.innerHTML = "Show Careers";
+    } else {
+        careers.style.display = "block";
+        toggle.innerHTML = "Hide Careers";
+    }
+};
