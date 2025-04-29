ce05350e1512abe9f7a8997b477cbbaa47050c2a
Rosa Myers <larosaardente@gmail.com>
2025-04-29 15:58:19 -0700

    Added a Text model to the domain model for tracking text blocks on the website for admin edit capabaility.

---
+++ JampotCapstoneWebApp/JampotCapstone/Models/TextElement.cs
@@ -0,0 +1,9 @@
+namespace JampotCapstone.Models;
+
+public class TextElement
+{
+    public int TextElementId { get; set; }
+    public string Name { get; set; } = "";
+    public string Content { get; set; } = "";
+    public string Webpage { get; set; } = "/Home";
+}
