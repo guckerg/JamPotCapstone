6d82559b830db304d1b327d567c84652649f9d4c
Rosa Myers <larosaardente@gmail.com>
2025-05-13 17:19:08 -0700

    Fixed bug in add new faq code.

--- JampotCapstoneWebApp/JampotCapstone/Models/TextElement.cs
+++ JampotCapstoneWebApp/JampotCapstone/Models/TextElement.cs
@@ -5,6 +5,6 @@ public class TextElement
     public int TextElementId { get; set; }
     public string Name { get; set; } = "";
     public string Content { get; set; } = "";
-    public Page? Page { get; set; }
+    public Page? Page { get; set; } = new Page ();
     public int PageId { get; set; }
 }
