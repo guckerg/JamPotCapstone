3c6e42a1b5d2c695ade256394c09414f42d9bbda
Gabe Gucker <guckerg.my.lanecc.edu>
2025-06-01 11:04:34 -0700

    prepping structure for careers unit tests

---
+++ JampotCapstoneWebApp/JampotUnitTests/CareersControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+using JampotCapstone.Controllers;
+using JampotCapstone.Data.Interfaces;
+using JampotCapstone.Models;
+using JampotCapstone.Models.ViewModels;
+
+namespace JampotUnitTests
+{
+    public class CareersControllerTests
+    {
+        [Fact]
+        public async Task AddApplication()
+        {
+
+        }
+    }
+}
